d996db94c9c929d557e74d693c5c84fd26bc64e0
Sidorova Anna <Anna@MacBooks-MacBook-Pro.local>
2021-08-02 15:44:42 +0300

    solutions for NotificationMessage & SortableTable components

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,64 @@
 export default class NotificationMessage {
+  static notification;
 
+  constructor(message, {
+    duration= 0,
+    type= 'success'
+              } = {}) {
+    if (NotificationMessage.notification) {
+      NotificationMessage.notification.remove();
+    }
+    this.message = message;
+    this.durationInSeconds = (duration / 1000) + 's';
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationInSeconds}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+  </div>
+    `
+  }
+
+  render(){
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+    NotificationMessage.notification = this.element;
+
+  }
+
+  show(parent = document.body){
+      parent.append(this.element)
+
+      setTimeout(() => {
+        this.remove();
+      }, this.duration)
+
+
+  }
+
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.notification = null;
+  }
 }
